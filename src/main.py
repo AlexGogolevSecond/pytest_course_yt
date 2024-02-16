8e306de049bc5af8bb21b3c484cbbd06b629210b
Alexandr Gogolev <a.gogolev@eteron.ru>
2024-02-16 23:54:43 +0500

    123

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
-class A:
-    x = 1
+from typing import Union
 
+
+class Calculator:
+    def devide(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        return x / y
+
+    def add(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        return x + y
+
+# class A:
+#     x = 1
+
+
+if __name__ == '__main__':
+    calculator = Calculator()
--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,33 @@
-from src.main import A
+# from src.main import A
 
 
-def test_main():
-    assert A.x == 1
+# def test_main():
+#     assert A.x == 1
 
-def test_2():
-    assert 2 == 2
+# def test_2():
+#     assert 2 == 2
 
 
-def test_sum():
-    x = 3
+# def test_sum():
+#     x = 3
+#     y = 2
+#     assert x + y == 5
+
+
+from src.main import Calculator
+import pytest
+
+
+# def test_divide():
+#     x = 1
+#     y = 2
+#     res = 0.5
+#     assert Calculator().devide(x, y) == res
+
+@pytest.mark.parametrize
+def test_divide():
+    x = 1
     y = 2
-    assert x + y == 5
+    res = 0.5
+    assert Calculator().devide(x, y) == res
+
